86409c0fce29781a8f36de5ac8ca9f1a16f915bb
Tubaktele <81114033+Tubaktele@users.noreply.github.com>
2021-06-02 11:22:09 +0300

    Add files via upload

--- Code.lua
+++ Code.lua
@@ -2,9 +2,9 @@ function Code(msg)
 local text = msg.content_.text_
 if Chat_Type == 'GroupBot' and ChekAdd(msg.chat_id_) == true then
 ----------------------------------------------------------------------------
-if text == ("مسح قائمه العام ℘") and DevTuBak(msg) then
+if text == ("مسح قائمه العام") and DevTuBak(msg) then
 database:del(bot_id.."TuBak:GBan:User")
-send(msg.chat_id_, msg.id_, "\n≼≽ تم مسح قائمه العام")
+send(msg.chat_id_, msg.id_, "\n◐ تم مسح قائمه العام")
 return false
 end
 end
